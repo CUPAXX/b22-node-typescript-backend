feb568ec6e6548faf4c25a922685a44c28fde810
cupaxx <fiqri.fa42@gmail.com>
2021-09-11 17:54:19 +0700

    timeout

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    connectTimeout: 90000
+    connectTimeout: 900000
 })
 
 
