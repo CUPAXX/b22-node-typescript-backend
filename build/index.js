3f1abe4b6dd3a54ac4e05c20f83108f2529641d9
cupaxx <fiqri.fa42@gmail.com>
2021-09-11 17:39:26 +0700

    rebuild

--- build/index.js
+++ build/index.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,11 +27,13 @@ var routes_1 = __importDefault(require("./routes"));
 var cors_1 = __importDefault(require("cors"));
 var morgan_1 = __importDefault(require("morgan"));
 var app = (0, express_1.default)();
+var dotenv = __importStar(require("dotenv"));
+dotenv.config();
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
 app.use((0, morgan_1.default)("dev"));
 app.use("/", routes_1.default);
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
     // tslint:disable-next-line: no-console
     console.log("App listening on port 8080");
 });
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import router from "./routes"
 import cors from "cors"
 import morgan from "morgan"
 const app = express()
+import * as dotenv from "dotenv"
+dotenv.config();
 
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(morgan("dev"))
 app.use("/", router)
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   // tslint:disable-next-line: no-console
   console.log("App listening on port 8080")
 })
